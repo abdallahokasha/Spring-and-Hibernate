9fe4d501b7d520baacc624e7aaa6439a72369027
okasha93 <abdookasha56@gmail.com>
2016-08-11 21:28:20 +0200

    Dependency Injection

--- SpringDemo/src/okasha/Triangle.java
+++ SpringDemo/src/okasha/Triangle.java
@@ -1,39 +1,36 @@
 package okasha;
 
 public class Triangle {
-	private String type;
-	private int height;
 
-	public int getHeight() {
-		return height;
+	private Point pointA, pointB, pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setHeight(int height) {
-		this.height = height;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-	public Triangle(String type) {
-		this.type = type;
+	public Point getPointB() {
+		return pointB;
 	}
 
-	public Triangle(int height) {
-		this.height = height;
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
 	}
 
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
+	public Point getPointC() {
+		return pointC;
 	}
 
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		System.out.println(getType() + " Triangle Drawn of height " + getHeight());
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
